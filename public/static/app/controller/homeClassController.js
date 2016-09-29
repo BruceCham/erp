beb98ff826f235d19aeaa829e234304bc554ce88
brucecham <brucecham@qq.com>
2016-09-30 00:44:10 +0800

    bugs

--- public/static/app/controller/homeClassController.js
+++ public/static/app/controller/homeClassController.js
@@ -1,6 +1,6 @@
 appLogin.controller('homeClassController',['$scope','$http','$stateParams',function($scope,$http,$stateParams){
 	$scope.dataArr = [];
-	$scope.activeCSindex = 0;//null 不显示，非null 显示
+	// $scope.activeCSindex = 0;//null 不显示，非null 显示
 	$scope.showResDetail = function(o,i){
 		// if( $scope.activeCSindex != null && $scope.activeCSindex != i) return;
 		// $scope.activeCSindex = $scope.activeCSindex == i?null:i;
@@ -19,13 +19,6 @@ appLogin.controller('homeClassController',['$scope','$http','$stateParams',funct
 	}).success(function(res){
 		if(res.resultCode == '000000'){
 			$scope.dataArr = res.result;
-			var o = $scope.dataArr[0];
-			$scope.activeCSindex = 0;
-			$scope.sn = o.sn || o.cs;
-			$scope.sp = o.sp;
-			$scope.op = o.op;
-			$scope.byMe = o.byMe;
-			// $scope.ResDetail = $scope.dataArr[0];
 		}
 	});
 }]);
